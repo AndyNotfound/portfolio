1d4e10bc6217bd0fa8088b6be3512ec723cb6c56
AndyNotfound <Andy.Notfound@gmail.com>
2023-04-20 00:37:10 +0700

    Add: Fix for 404 page that I forgot to delete

--- pages/404.js
+++ pages/404.js
@@ -1,22 +1,16 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function FourOhFour() {
   return (
-    <>
-      <Navbar />
-      <section className="notfound">
-        <Image src="/assets/notfound.svg" width={600} height={600} alt="" />
-        <div className="notfoundDetail">
-          <h1 className="page-header">404 Not Found</h1>
-          <Link href="/projects" className="primary-button">
-            Find Projects
-          </Link>
-        </div>
-      </section>
-      <Footer />
-    </>
+    <section className="notfound">
+      <Image src="/assets/notfound.svg" width={600} height={600} alt="" />
+      <div className="notfoundDetail">
+        <h1 className="page-header">404 Not Found</h1>
+        <Link href="/projects" className="primary-button">
+          Find Projects
+        </Link>
+      </div>
+    </section>
   );
 }
